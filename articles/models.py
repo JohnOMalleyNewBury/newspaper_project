7888e55d7348a7302bb22ff9848f3a94b749d709
JohnOMalleyNewBury <john.omalley@newbury.edu>
2018-12-13 22:25:48 -0500

    chapter 15 is done

--- articles/models.py
+++ articles/models.py
@@ -20,12 +20,13 @@ class Article(models.Model):
         return reverse('article_detail', args=[str(self.id)])
 
 
-class Comment(models.Model): # new
-    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE,related_name='comments',)
     comment = models.CharField(max_length=140)
     author = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE,
+        related_name='comments',
     )
 
     def __str__(self):
